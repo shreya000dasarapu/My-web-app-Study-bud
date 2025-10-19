64e2ff7ac27823f40ee1330c15934b844135eb37
shreya000dasarapu <shhreya000@gmail.com>
2025-10-19 14:17:27 +0530

    Added update, delete and search options

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
+from django.db.models import Q
 from .forms import RoomForm
 # rooms=[
 #     {'id': 1, 'name':'Lets learn python'},
@@ -9,8 +10,15 @@ from .forms import RoomForm
 # ]
 # Create your views here.
 def home(request):
-    rooms=Room.objects.all()
-    context = {'rooms':rooms}
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=q)|
+        Q(name__icontains=q)|
+        Q(description__icontains=q)
+        )
+    topics=Topic.objects.all()
+    room_count=rooms.count()
+    context = {'rooms':rooms, 'topics':topics , 'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
@@ -39,4 +47,11 @@ def updateRoom(request,pk):
 
     context={'form':form}
     return render(request,'base/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
 
